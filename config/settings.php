da60fc3a6f65b99173c156b6183103aabcbcfbfd
Brad Runnels <runnels.brad@gmail.com>
2021-05-11 21:33:40 -0400

    change default PDO fetch mode to FETCH_OBJ

--- config/settings.php
+++ config/settings.php
@@ -37,8 +37,8 @@ $settings['db'] = [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         // Emulate prepared statements
         PDO::ATTR_EMULATE_PREPARES => true,
-        // Set default fetch mode to array
-        // PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        // Set default fetch mode to object
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
         // Set character set
         PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci'
     ]
