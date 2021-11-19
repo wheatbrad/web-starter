f23f66668a2fbbcd64300919a3c6bae299fce0a3
Microsoft Learn Student <learn@contoso.com>
2021-11-18 20:01:28 -0500

    rm app creation from container definitions

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,22 +1,30 @@
 <?php
 
 use DI\ContainerBuilder;
-use Slim\App;
-use Slim\Middleware\ErrorMiddleware;
+use Slim\Factory\AppFactory;
 
 require APP_ROOT.'/vendor/autoload.php';
 
 $containerBuilder = new ContainerBuilder();
 $containerBuilder->addDefinitions(__DIR__.'/container.php');
 $container = $containerBuilder->build();
-$app = $container->get(App::class);
+
+AppFactory::setContainer($container);
+$app = AppFactory::create();
 
 // Register routes
 (require APP_ROOT.'/app/routes.php')($app);
 
+// Get error settings from container
+$e = $container->get('settings')['error'];
+
 // Register middleware
 $app->addBodyParsingMiddleware();
-$app->addRoutingMiddleware();    
-$app->add(ErrorMiddleware::class);
+$app->addRoutingMiddleware();
+$app->addErrorMiddleware(
+    $e['display_error_details'],
+    $e['log_errors'],
+    $e['log_error_details'],
+);
 
 return $app;
--- config/container.php
+++ config/container.php
@@ -2,35 +2,12 @@
 
 use Fullpipe\TwigWebpackExtension\WebpackExtension;
 use Psr\Container\ContainerInterface;
-use Psr\Http\Message\ResponseFactoryInterface;
-use Slim\App;
-use Slim\Factory\AppFactory;
-use Slim\Middleware\ErrorMiddleware;
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
 
 return [
     'settings' => function () {
         return require __DIR__.'/settings.php';
-    },
-
-    App::class => function (ContainerInterface $container) {
-        AppFactory::setContainer($container);
-
-        return AppFactory::create();
-    },
-
-    ErrorMiddleware::class => function (ContainerInterface $container) {
-        $app = $container->get(App::class);
-        $settings = $container->get('settings')['error'];
-
-        return new ErrorMiddleware(
-            $app->getCallableResolver(),
-            $app->getResponseFactory(),
-            (bool) $settings['display_error_details'],
-            (bool) $settings['log_errors'],
-            (bool) $settings['log_error_details']
-        );
     },
 
     Environment::class => function () {
@@ -60,9 +37,5 @@ return [
         $dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
         
         return new PDO($dsn, $username, $password, $flags);
-    },
-
-    ResponseFactoryInterface::class => function (ContainerInterface $container) {
-        return $container->get(App::class)->getResponseFactory();
     }
 ];
