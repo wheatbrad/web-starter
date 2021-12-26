a63b7c7072967a926303c67cce5129d416f8715c
Brad Runnels <bradrunnels@Brads-M1-Max-MacBook-Pro.local>
2021-12-26 17:30:26 -0500

    simply routes for demo: home page (/), protected resource (/protected), login endpoint (/user/login), logout endpoint (/user/logout)

--- app/routes.php
+++ app/routes.php
@@ -1,13 +1,14 @@
 <?php
 
 use App\Controller\PageController;
+use App\Controller\UserController;
 use Slim\App;
 
 return function (App $app) {
 
     $app->get('/', [PageController::class, 'home']);
-    $app->get('/login', [PageController::class, 'login']);
-    $app->get('/logout', [PageController::class, 'logout']);
     $app->get('/protected', [PageController::class, 'protected']);
+    $app->get('/user/login', [UserController::class, 'login']);
+    $app->get('/user/logout', [UserController::class, 'logout']);
 
 };
